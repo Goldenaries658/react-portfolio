a9a74d81a6803231acb0c9db058e15470db1e47b
Goldenaries658 <59972622+Goldenaries658@users.noreply.github.com>
2020-08-23 18:49:19 +0100

    Created header with nav

---
+++ src/components/Header.js
@@ -0,0 +1,97 @@
+import React, { useState } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import LibraryBooksIcon from '@material-ui/icons/LibraryBooks';
+import InfoIcon from '@material-ui/icons/Info';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  list: {
+    width: 250,
+  },
+}));
+
+export default function SearchAppBar() {
+  const classes = useStyles();
+
+  const [drawer, setDrawer] = useState({ isOpen: false });
+
+  const toggleDrawer = (newState) => (event) => {
+    if (
+      event &&
+      event.type === 'keydown' &&
+      (event.key === 'Tab' || event.key === 'Shift')
+    ) {
+      return;
+    }
+    console.log(`setting drawer to ${newState}`);
+
+    setDrawer({ isOpen: newState });
+  };
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="open drawer"
+            onClick={toggleDrawer(true)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography className={classes.title} variant="h6" noWrap>
+            Houston
+          </Typography>
+        </Toolbar>
+        <SwipeableDrawer
+          anchor="left"
+          open={drawer.isOpen}
+          onClose={toggleDrawer(false)}
+          onOpen={toggleDrawer(true)}
+        >
+          <div
+            className={classes.list}
+            role="presentation"
+            onClick={toggleDrawer(false)}
+            onKeyDown={toggleDrawer(false)}
+          >
+            <List>
+              {['About Me', 'Portfolio'].map((text, index) => (
+                <ListItem button key={text}>
+                  <ListItemIcon>
+                    {index === 0 ? <InfoIcon /> : <LibraryBooksIcon />}
+                  </ListItemIcon>
+                  <ListItemText primary={text} />
+                </ListItem>
+              ))}
+            </List>
+          </div>
+        </SwipeableDrawer>
+      </AppBar>
+    </div>
+  );
+}
