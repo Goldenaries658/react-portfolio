243fae81a755b75d65e1bf53fd33b061ffa0e35f
Goldenaries658 <59972622+Goldenaries658@users.noreply.github.com>
2020-08-23 23:37:59 +0100

    Styling

--- src/App.js
+++ src/App.js
@@ -1,33 +1,45 @@
 import React from 'react';
 import './app.css';
 import Header from './components/Header';
+import AboutMe from './pages/AboutMe';
+import purple from '@material-ui/core/colors/purple';
 import {
   ThemeProvider,
   createMuiTheme,
-  CssBaseline,
-  Container,
-} from '@material-ui/core';
-import { purple } from '@material-ui/core/colors';
+  makeStyles,
+} from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
+import Paper from '@material-ui/core/Paper';
 
 const theme = createMuiTheme({
   palette: {
     type: 'dark',
-    primary: {
-      main: purple[500],
-    },
+    primary: purple,
   },
 });
 
+const useStyles = makeStyles(() => ({
+  root: {
+    paddingTop: '12px',
+    paddingBottom: '12px',
+    backgroundColor: '#42424290',
+    backdropFilter: 'blur(3px) brightness(200%)',
+  },
+}));
+
 export default function App() {
+  const classes = useStyles();
+
   return (
     <div className="App">
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <Header />
         <br />
-        {/* <Container>
-          
-        </Container> */}
+        <Container className={classes.root} component={Paper}>
+          <AboutMe />
+        </Container>
       </ThemeProvider>
     </div>
   );
