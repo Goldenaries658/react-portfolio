3e75bc7495bfa3870658e5618bf610ed4e52a360
Goldenaries658 <59972622+Goldenaries658@users.noreply.github.com>
2020-08-23 18:48:39 +0100

    Theming

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import React from 'react';
-import './app.css'
+import './app.css';
+import Header from './components/Header';
+import {
+  ThemeProvider,
+  createMuiTheme,
+  CssBaseline,
+  Container,
+} from '@material-ui/core';
+import { purple } from '@material-ui/core/colors';
+
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+    primary: {
+      main: purple[500],
+    },
+  },
+});
 
 export default function App() {
   return (
     <div className="App">
-      
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Header />
+        <br />
+        {/* <Container>
+          
+        </Container> */}
+      </ThemeProvider>
     </div>
   );
 }
