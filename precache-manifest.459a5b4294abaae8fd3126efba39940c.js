748dee58158b49ecbf1e92e83df430b6fef117d6
Goldenaries658 <59972622+Goldenaries658@users.noreply.github.com>
2020-08-24 23:18:25 +0100

    Updates

---
+++ precache-manifest.459a5b4294abaae8fd3126efba39940c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23ed0758f7f05f5a60906f9afb86341a",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "4e31d144505569c57dd5",
+    "url": "/react-portfolio/static/css/main.e2f831a1.chunk.css"
+  },
+  {
+    "revision": "692100b06af7cdd2f6bb",
+    "url": "/react-portfolio/static/js/2.47999555.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/react-portfolio/static/js/2.47999555.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e31d144505569c57dd5",
+    "url": "/react-portfolio/static/js/main.6e294ee2.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "d328d74b1ccfce52fa40098868c21752",
+    "url": "/react-portfolio/static/media/fitness-buddy.d328d74b.webp"
+  },
+  {
+    "revision": "de5b49bebeba30425a8bae86bd591fb7",
+    "url": "/react-portfolio/static/media/javascript-quiz.de5b49be.webp"
+  },
+  {
+    "revision": "11c1f35701e4fbb9b28516388be1ee49",
+    "url": "/react-portfolio/static/media/profile-pic.11c1f357.webp"
+  },
+  {
+    "revision": "5319a9064392faeabdee8e80a0a6884a",
+    "url": "/react-portfolio/static/media/the-music-app.5319a906.webp"
+  },
+  {
+    "revision": "a08a19ee21e1b0df4a6e647eadc7d7c8",
+    "url": "/react-portfolio/static/media/topography.a08a19ee.svg"
+  },
+  {
+    "revision": "307515f8433848687a028bb23a3e6dde",
+    "url": "/react-portfolio/static/media/weather-app.307515f8.webp"
+  }
+]);
