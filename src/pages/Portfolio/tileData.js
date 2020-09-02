dc6cd55a92e21d9d54782c0bb4b092a5c19040ac
Goldenaries658 <59972622+Goldenaries658@users.noreply.github.com>
2020-09-02 16:34:19 +0100

    Improved compatibility of images to support all devices

--- src/pages/Portfolio/tileData.js
+++ src/pages/Portfolio/tileData.js
@@ -1,29 +1,45 @@
-import fitnessBuddyImg from './images/fitness-buddy.webp';
-import javaScriptQuizImg from './images/javascript-quiz.webp';
-import musicAppImg from './images/the-music-app.webp';
-import weatherAppImg from './images/weather-app.webp';
+import fitnessBuddyWebp from './images/fitness-buddy.webp';
+import fitnessBuddyJpeg from './images/fitness-buddy.jpg';
+import javaScriptQuizWebp from './images/javascript-quiz.webp';
+import javaScriptQuizJpeg from './images/javascript-quiz.jpg';
+import musicAppWebp from './images/the-music-app.webp';
+import musicAppJpeg from './images/the-music-app.jpg';
+import weatherAppWebp from './images/weather-app.webp';
+import weatherAppJpeg from './images/weather-app.jpg';
 
 export const tileData = [
   {
-    img: fitnessBuddyImg,
+    img: {
+      webp: fitnessBuddyWebp,
+      jpeg: fitnessBuddyJpeg,
+    },
     title: 'Fitness Buddy',
     description: 'On demand training guides.',
     href: 'https://github.com/JoeDodgson/Fitness-buddy',
   },
   {
-    img: musicAppImg,
+    img: {
+      webp: musicAppWebp,
+      jpeg: musicAppJpeg,
+    },
     title: 'The Music App',
     description: 'Search for all things music.',
     href: 'https://github.com/meeday/The-Music-App',
   },
   {
-    img: javaScriptQuizImg,
+    img: {
+      webp: javaScriptQuizWebp,
+      jpeg: javaScriptQuizJpeg,
+    },
     title: 'JavaScript Quiz',
     description: 'A basic quiz on JavaScript fundamentals',
     href: 'https://github.com/Goldenaries658/JavaScript-Quiz/',
   },
   {
-    img: weatherAppImg,
+    img: {
+      webp: weatherAppWebp,
+      jpeg: weatherAppJpeg,
+    },
     title: 'The Weather App',
     description: 'A simple weather forecasting webapp.',
     href: 'https://github.com/Goldenaries658/Weather-App',
